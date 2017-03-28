80a307775c47cea34ed679201d713158a5a0c7b4
DK\nim <nekit.mc@gmail.com>
2017-03-28 19:19:21 -0400

    Initial commit

---
+++ src/Sitecore.Support.154275/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.154275")]
+[assembly: AssemblyProduct("Sitecore.Support.154275")]
+[assembly: ComVisible(false)]
